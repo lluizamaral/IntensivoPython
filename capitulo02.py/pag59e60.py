67180117f6d93e66ee0263263e1c6977332a6e9b
zkTs <iamthezkghost@gmail.com>
2022-08-19 20:38:31 -0300

    Add files via upload
    teste 2

---
+++ capitulo02.py/pag59e60.py
@@ -0,0 +1,15 @@
+# explicação pag 58 a 60
+name = "ada lovelace"
+print(name.title())
+# "name" é salva como uma variavel e o "ponto(.)"indica que o title() deve agir junto á variavel.
+#e xistem outros comandos como: #
+print(name.upper())
+name = "ADA LoVeLAce"
+print(name.lower())
+# complementacao de variaveis:
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+# sitiacao de exemplo:
+print("welcome to our site" + " " + full_name.title() + "!!!")
---
+++ capitulo02.py/pag70.py
@@ -0,0 +1,16 @@
+# pag 70
+print("\t2.8.exercicios")
+print(2+2+2+2)
+print(12-4)
+print(4*2)
+print(24/3)
+
+print("\t2.9.exercicios")
+numero_favorito = str("9")
+print(numero_favorito + ",esse é meu numero favorito!!!")
+
+# 2.10.exercicios  #
+# 19/07 
+# luiz gustavo ramos amaral
+
+print("\t2.11.exercicios") 
