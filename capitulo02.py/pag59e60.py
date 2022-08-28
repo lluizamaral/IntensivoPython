af2a1730599b5581f2783aa3c24bd2cc757cfe74
zkTs <iamthezkghost@gmail.com>
2022-08-28 01:56:16 -0300

    Add files via upload

---
+++ capitulo02.py/pag59e60.py
@@ -0,0 +1,24 @@
+# explicação pag 58 a 60
+
+
+
+name = "ada lovelace"
+print(name.title())                          #----------- "name" é salva como uma variavel e o "ponto(.)"indica que o title() deve agir junto á variaveL
+print(name.upper())
+
+
+name = "ADA LoVeLAce"
+print(name.lower())
+
+
+
+first_name = "ada"                 
+last_name = "lovelace"
+full_name = first_name + " " + last_name            #---------------complementacao de variaveis que pode ser usadas para um exemplo de login com nome completo
+print(full_name)
+
+
+
+
+#  FORMATAÇÃO  #
+print("welcome to our site" + " " + full_name.title() + "!!!")
---
+++ capitulo02.py/resumo.py
@@ -0,0 +1,25 @@
+
+
+
+usuarios = []
+sobrenomes = []
+
+
+nome = "eric"
+print(".""ola" + " " + nome + " " + "vamos aprender python hoje?")
+print("." + nome.title())                                                  #-------------ISERÇÃO DOS COMANDOS title,lower,upper . 
+
+
+    #.----------------//---------------------.#
+usuarios.append("marcos"),sobrenomes.append("vinicios")                    #------------APRESENTAÇAO DO COMANDO APPEND (adicona o valor á algo)             
+
+
+    #-----------------//------------------#
+nome_03 = " luiz " + "gustavo"
+nome_04 = " alexandre" + " " + "ramos"
+nome_05 = "leandro " + "amaral"
+print("\t2.7.nomes:".upper())
+print("\n" + nome_03.strip())                                 #----------USO DO COMANDO STRIP PARA REMOVER ESPAÇOS NAS DIGITAÇÕES 
+print("\n" + nome_04.lstrip()) 
+print("\n" + nome_05.rstrip())
+
